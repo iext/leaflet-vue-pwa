0941649fe1c1fbcd5b548b261c723bd65be99d16
alex <milickij@gmail.com>
2020-09-03 15:02:44 +0500

    Меняет карту с OL на Leaflet

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -3,25 +3,16 @@
 </template>
 
 <script>
-import "ol/ol.css";
-import { Map, View } from "ol";
-import TileLayer from "ol/layer/Tile";
-import OSM from "ol/source/OSM";
+import L from "leaflet";
 
 export default {
   mounted() {
-    new Map({
-      target: "map",
-      layers: [
-        new TileLayer({
-          source: new OSM(),
-        }),
-      ],
-      view: new View({
-        center: [0, 0],
-        zoom: 0,
-      }),
-    });
+    var map = L.map("map").setView([61.254, 73.397], 13);
+
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
   },
 };
 </script>
@@ -30,27 +21,5 @@ export default {
 #map {
   width: 100%;
   height: 100vh;
-}
-
-.ol-zoom {
-  left: unset;
-  right: 12px;
-  top: 70%;
-  transform: translateY(-70%);
-}
-
-.ol-zoom .ol-zoom-in {
-  margin-bottom: 5px;
-  border-radius: 50%;
-  width: 50px;
-  height: 50px;
-  font-size: 15pt;
-}
-.ol-zoom .ol-zoom-out {
-  border-radius: 50%;
-  margin-left: 5px;
-  width: 40px;
-  height: 40px;
-  font-size: 12pt;
 }
 </style>
